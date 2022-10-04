ee0d14e8c9f89ebc168f1fc3016a98a018ddda8d
apisceva <aljona.gromova@gmail.com>
2022-10-04 04:23:29 -0400

    added useEffect (with fetchData function) in component StockList

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,3 +1,22 @@
+import { useState, useEffect } from "react";
+import finnHub from "../apis/finnHub";
+
 export const StockList = () => {
+  const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await finnHub.get("/quote", {
+          params: {
+            symbol: "MSFT",
+          },
+        });
+        console.log(response);
+      } catch (err) {}
+    };
+    fetchData();
+  }, []);
+
   return <div>StockList</div>;
 };
