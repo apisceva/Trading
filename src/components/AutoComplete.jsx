8474d88d75a0bb25cd68d8befdab4d5a0046b8a2
apisceva <aljona.gromova@gmail.com>
2022-10-05 07:19:34 -0400

    added useEffect (search)

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,7 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import finnHub from "../apis/finnHub";
 
 export const Autocomplete = () => {
   const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async () => {
+      try {
+        const response = await finnHub.get("/search", {
+          params: {
+            q: search,
+          },
+        });
+        if (isMounted) {
+          setResults(response.data);
+        }
+      } catch (err) {}
+      if (search.length > 0) {
+        fetchData();
+      } else {
+        setResults([]);
+      }
+    };
+    return () => (isMounted = false);
+  }, [search]);
+
   return (
     <div className="w-50 p-5 rounded mx-auto">
       <div className="form-floating dropdown">
