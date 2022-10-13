dfb81a97280de74862cfaa3582cf1a471b77e43d
apisceva <aljona.gromova@gmail.com>
2022-10-13 05:24:53 -0400

    added 3 buttons 24h 7d and 1y

--- src/components/StockChart.jsx
+++ src/components/StockChart.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Chart from "react-apexcharts";
 
 export const StockChart = ({ chartData, symbol }) => {
-  const { dateFormat, setDateFormat } = useState("24h");
+  const [dateFormat, setDateFormat] = useState("24h");
   const { day, week, year } = chartData;
   const options = {
     title: {
@@ -47,16 +47,40 @@ export const StockChart = ({ chartData, symbol }) => {
   const series = [
     {
       name: symbol,
-      data: determineTimeFormat,
+      data: determineTimeFormat(),
     },
   ];
+
+  const renderButtonSelect = (button) => {
+    const classes = "btn m-1";
+    if (button === dateFormat) {
+      return classes + "btn-primary";
+    } else {
+      return classes + "btn=outline-primary";
+    }
+  };
   return (
     <div className="mt-5 p-4 shadow-sm bg-white">
       <Chart options={options} series={series} type="area" width="100%" />
       <div>
-        <button onClick={() => setDateFormat("24h")}>24h</button>
-        <button onClick={() => setDateFormat("7d")}>7d</button>
-        <button onClick={() => setDateFormat("1y")}>1y</button>
+        <button
+          className={renderButtonSelect("24h")}
+          onClick={() => setDateFormat("24h")}
+        >
+          24h
+        </button>
+        <button
+          className={renderButtonSelect("7d")}
+          onClick={() => setDateFormat("7d")}
+        >
+          7d
+        </button>
+        <button
+          className={renderButtonSelect("1y")}
+          onClick={() => setDateFormat("1y")}
+        >
+          1y
+        </button>
       </div>
     </div>
   );
--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -75,7 +75,6 @@ export const StockDetailPage = () => {
       {chartData && (
         <div>
           <StockChart chartData={chartData} symbol={symbol} />
-          <StockData symbol={symbol} />
         </div>
       )}
     </div>
