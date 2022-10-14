d1ac38ff10df1a96cd1444014d48b026fee42a84
apisceva <aljona.gromova@gmail.com>
2022-10-14 06:20:42 -0400

    added png to StockOverViewPage

--- src/pages/StockOverViewPage.jsx
+++ src/pages/StockOverViewPage.jsx
@@ -1,9 +1,13 @@
 import { Autocomplete } from "../components/AutoComplete";
 import { StockList } from "../components/StockList";
+import trading from "../images/Trading.png";
 
 export const StockOverViewPage = () => {
   return (
     <div>
+      <div className="text-center">
+        <img src={trading} />
+      </div>
       <Autocomplete />
       <StockList />
     </div>
